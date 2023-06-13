969d9655ad79dc4020efa7654aca681d2e10efa7
Ali-Ahmed18 <aliahmedyk18@gmail.com>
2023-06-13 18:45:24 +0500

    Completed

--- app.js
+++ app.js
@@ -1,21 +1,61 @@
-function foo(upt){
+var table = document.getElementById("table")
+var getUser =JSON.parse(localStorage.getItem("arr"))
+if(getUser !== null){
+  
+    for (index of getUser) {
+        var i = (table.children.length)
+        var trElement = document.createElement("tr")
+        trElement.innerHTML = "<td>"+ i +"</td><td>"+ index.name.toUpperCase() +"</td> <td>"+ index.class +"</td><td><button type='button' onclick='update(this)'>Update</button></td><td><button onclick='del(this)'>Delete</button></td>"
+        table.appendChild(trElement)
+    }
+    
+}
+function foo(){
+    var getUser =JSON.parse(localStorage.getItem("arr"))
     var name = document.getElementById("name")
     var clas = document.getElementById("class")
-    var table = document.getElementById("table")
     var trElement = document.createElement("tr")
     var i = (table.children.length)
-    if(upt == "upt"){
-
+    var obj ={
+        name : name.value,
+        class : clas.value
+    }
+    if(getUser == null){
+        var arr = []
+        arr.push(obj)
+        localStorage.setItem("arr",JSON.stringify(arr))
+        trElement.innerHTML = "<td>"+ i +"</td><td>"+ name.value.toUpperCase() +"</td> <td>"+ clas.value +"</td><td><button type='button' onclick='update(this)'>Update</button></td><td><button onclick='del(this)'>Delete</button></td>"
+        table.appendChild(trElement)
+         name.value = ""
+         clas.value = ""
     }else{
-            trElement.innerHTML = "<td>"+ i +"</td><td>"+ name.value.toUpperCase() +"</td> <td>"+ clas.value +"</td><td><button type='button' onclick='update(this)'>Update</button></td><td><button onclick='del(this)'>Delete</button></td>"
-   table.appendChild(trElement)
-    name.value = ""
-    clas.value = ""
-    }   
+        getUser.push(obj)
+        localStorage.setItem("arr",JSON.stringify(getUser))
+        trElement.innerHTML = "<td>"+ i +"</td><td>"+ name.value.toUpperCase() +"</td> <td>"+ clas.value +"</td><td><button type='button' onclick='update(this)'>Update</button></td><td><button onclick='del(this)'>Delete</button></td>"
+        table.appendChild(trElement)
+         name.value = ""
+         clas.value = ""
+         
+    }
+   
+
+    
 }
 function del(element){
-    element.parentNode.parentNode.remove()
-}
+    var getUser = JSON.parse(localStorage.getItem("arr"))
+        for (i= 0; i < getUser.length; i++) {
+            
+                if(getUser[i].name.toUpperCase() == element.parentNode.parentNode.children[1].innerHTML && getUser[i].class == element.parentNode.parentNode.children[2].innerHTML){
+                    getUser.splice(i,1)
+                    localStorage.setItem("arr",JSON.stringify(getUser))
+                }
+        }
+        element.parentNode.parentNode.remove()
+        for(i = 1;i< table.children.length; i++){
+        table.children[i].children[0].innerHTML = i
+        }
+    }
+
 
 var upDate = document.getElementById("update")
 var main = document.getElementsByClassName("main")
